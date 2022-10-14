85bcdf8e5c0a4978eecaa99f840a32e615c7f76a
Tob <67708830+Tobbin23@users.noreply.github.com>
2022-10-14 23:00:41 +0200

    Add files via upload

--- gui.py
+++ gui.py
@@ -1,164 +1,338 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
-from decimal import Decimal
-import time
-from utils import dotaz, czk
-# 	res1=(float(entry1.get()) / float(czk))/ btc
-# 	sav = entry2.insert(0, res1)
 
+from decimal import Decimal
+from exit_delete import krypto_seznam, open_url
+import time
+import os
+from utils import dotaz, czk
+#   res1=(float(entry1.get()) / float(czk))/ btc
+#   sav = entry2.insert(0, res1)
+
+
+def refresh():
+    ws.destroy()
+    os.system("python3"+"  "+os.path.join(os.path.dirname(os.path.realpath((__file__))),"gui.py"))
+    ws.mainloop()
 def exit():
-	ws.destroy()
+    ws.destroy()
 def smaz():
-	entry1.delete(0, tk.END)
-	entry2.delete(0, tk.END)
+    entry1.delete(0, tk.END)
+    entry2.delete(0, tk.END)
 
 def krypto_fiat():
-	""" Prevod z Bitcoinu na Fiat"""
-	entry1.delete(0, tk.END)
-	if entry2.get() != "":
-		try:
-			entry1.delete(0, tk.END)
-			ent=czk(satoshi=float(entry2.get()))
-			entry1.insert(0, en)
-			#entry1.insert(0,"{:,.1f}".format(ent))
-		except:
-			entry1.delete(0, tk.END)
-			en = czk(satoshi=(entry2.get()))
-			entry1.insert(0, en)
-	else:
-		entry2.insert(0, "nezdano")
-		messagebox.showerror("Hodnota", "nezadana hodnota")
-		entry2.delete(0, tk.END)
+    """ Prevod z Bitcoinu na Fiat"""
+    entry1.delete(0, tk.END)
+    if entry2.get() != "":
+        try:
+            entry1.delete(0, tk.END)
+            ent=czk(satoshi=float(entry2.get()))
+            entry1.insert(0, en)
+            #entry1.insert(0,"{:,.1f}".format(ent))
+        except:
+            entry1.delete(0, tk.END)
+            en = czk(satoshi=(entry2.get()))
+            entry1.insert(0, en)
+    else:
+        entry2.insert(0, "nezdano")
+        messagebox.showerror("Hodnota", "nezadana hodnota")
+        entry2.delete(0, tk.END)
 
 
 def fiat_krypto():
-	global kz, btc
-	""" Prevod z Fiat meny na  Bitcoin"""
-	entry2.delete(0, tk.END)
-	if entry1.get() != "":
-		try:
-			
-			en = float(entry1.get())
-			#listbox.get(listbox.curselection()),
-			
-			c,k,d,p,czk,bt = dotaz(castka=round(float(en)),mena1=listbox.get(),COIN=krypto_list.get().lower(), fiatnaKrypto=True)
-			castkaA.set(str(c)) # castka vkladu
-			kurzA.set(str(k))  # jaky je kurz czk
-			kurzB.set(str(listbox.get())) # jakou menu si vyzadal uzivatel
-			kurzb.set(str(d)) # jaky je kuzr dane meny
-			entry2.insert(0,str(bt))
-			
-		except:
-			entry2.delete(0, tk.END)
-			en =entry1.get().replace(",","")
-			c, z ,b,btc= dotaz(castka=round(float(en)),mena1=listbox.get(), COIN=krypto_list.get().lower(),fiatnaKrypto=True)
-			castkaA.set(str(c)) # castka vkladu
-			kurzA.set(str(z))  # jaky je kurz czk
-			kurzB.set(str(listbox.get())) # jakou menu si vyzadal uzivatel
-			kurzb.set(str(b)) # jaky je kuzr dane meny
-			entry2.insert(0,str(btc))
-# 		ent = (float(entry1.get())/czk)/str(btc)
-# 		entry2.insert(0,round(Decimal(ent),18))
-	else:
-		entry1.insert(0, "nezadano")
-		messagebox.showerror("Hodnota", "nezadana hodnota")
-		entry1.delete(0, tk.END)
-		
-		
-ws = tk.Tk()
+    global kz, btc
+    """ Prevod z Fiat meny na  Bitcoin"""
+    entry2.delete(0, tk.END)
+    if entry1.get() != "":
+        try:
+            
+            en = float(entry1.get())
+            #listbox.get(listbox.curselection()),
+            
+            c,k,d,p,czk,bt = dotaz(castka=round(float(en)),mena1=listbox.get(),COIN=krypto_list.get().lower(), fiatnaKrypto=True)
+            castkaA.set(str(c)) # castka vkladu
+            kurzA.set(str(k))  # jaky je kurz czk
+            kurzB.set(str(listbox.get())) # jakou menu si vyzadal uzivatel
+            kurzb.set(str(d)) # jaky je kuzr dane meny
+            entry2.insert(0,str(bt))
+            
+        except:
+            entry2.delete(0, tk.END)
+            en =entry1.get().replace(",","")
+            c, z ,b,btc= dotaz(castka=round(float(en)),mena1=listbox.get(), COIN=krypto_list.get().lower(),fiatnaKrypto=True)
+            castkaA.set(str(c)) # castka vkladu
+            kurzA.set(str(z))  # jaky je kurz czk
+            kurzB.set(str(listbox.get())) # jakou menu si vyzadal uzivatel
+            kurzb.set(str(b)) # jaky je kuzr dane meny
+            entry2.insert(0,str(btc))
+#       ent = (float(entry1.get())/czk)/str(btc)
+#       entry2.insert(0,round(Decimal(ent),18))
+    else:
+        entry1.insert(0, "nezadano")
+        messagebox.showerror("Hodnota", "nezadana hodnota")
+        entry1.delete(0, tk.END)
+        
+        
+#ws = ThemedTk(theme="aquativo")
+ws= tk.Tk()
 ws.title("Python Guides")
-ws.geometry("830x600")
-
-# seznam = ("CZK", "EUR", "CHF")
-# listbox = tk.Listbox(ws, width=12, height=12)
-# for meny in seznam:
-# 	listbox.insert(tk.END, meny)
-seznam_fiat = ['AUD','GBP','CNY','DKK','EUR','HRK','JPY','CAD','HUF','NOK','PLN','RON','SEK','CHF','TRY','USD']
-frame = tk.Frame(ws,height=50)
-frame.place(x = 15, y = 26)
-listbox = ttk.Combobox(frame,state = "readonly",width=8, values = seznam_fiat,font=("Arial", 15))
-listbox.set("Fiat")
-listbox.pack(padx = 1, pady=1)
-
-seznam_krypto = ['BTCUSD','BTCEUR','BTCGBP','BTCPAX','GBPUSD','GBPEUR','EURUSD','XRPUSD','XRPEUR'
-				 ,'XRPGBP','LTCUSD','LTCEUR','LTCGBP','ETHUSD','ETHEUR',
-				 'ETHGBP','BCHUSD','BCHEUR','PAXUSD','XLMUSD',
-				 'XLMGBP','LINKUSD','LINKEUR','LINKGBP','OMGUSD','OMGEUR','OMGGBP',
-				 'USDCUSD','USDCEUR','AAVEUSD','AAVEEUR',
-				 'BATUSD','BATEUR','UMAUSD','UMAEUR','DAIUSD','KNCUSD','KNCEUR','MKRUSD',
-				 'ZRXUSD','ZRXEUR','GUSDUSD','ALGOUSD','ALGOEUR','AUDIOUSD'
-				 ,'CRVUSD','CRVEUR','SNXUSD','SNXEUR','UNIUSD','UNIEUR',
-				 'YFIEUR','COMPUSD','COMPEUR','GRTUSD','GRTEUR','LRCUSD','LRCEUR','USDTUSD',
-				 'USDCUSDT','BTCUSDT','ETHUSDT','XRPUSDT','EURTEUR','EURTUSD','MANAUSD',
-				 'MATICUSD','MATICEUR','SUSHIUSD','SUSHIEUR','CHZUSD','CHZEUR','ENJUSD','ENJEUR',
-				 'HBAREUR','ALPHAUSD','ALPHAEUR','AXSUSD','AXSEUR','FTTUSD','FTTEUR','SANDUSD',
-				 'STORJUSD','STORJEUR','ADAUSD','ADAEUR','FETUSD','FETEUR','SKLUSD','SKLEUR',
-				 'CELEUR','SLPUSD','SLPEUR','SXPUSD','SXPEUR','SGBUSD','SGBEUR','AVAXUSD','AVAXEUR',
-				 'DYDXEUR','FTMUSD','FTMEUR','SHIBUSD','SHIBEUR','AMPUSD','AMPEUR','ENSUSD','ENSEUR',
-				 'GALAEUR','PERPUSD','PERPEUR','CTSIUSD','CTSIEUR','CVXUSD','CVXEUR','IMXUSD',
-				 'NEXOUSD','NEXOEUR','ANTUSD','ANTEUR','GODSUSD','GODSEUR','RADUSD','RADEUR','BANDUSD',
-				 'INJUSD','INJEUR','RLYUSD','RLYEUR','RNDRUSD','RNDREUR','VEGAUSD','VEGAEUR','1INCHUSD',
-				 'SOLUSD','SOLEUR','APEUSD','APEEUR','MPLUSD','MPLEUR','DOTUSD','DOTEUR','NEARUSD','NEAREUR']
-frame = tk.Frame(ws,height=50)
-frame.place(x=670, y=26)
-krypto_list = ttk.Combobox(frame,state = "readonly",width=8, values = seznam_krypto,font=("Arial", 15))
-krypto_list.set("Krypto")
-krypto_list.pack(padx = 1, pady=1)
-
 ws.resizable(False, False) # nastaveni nemene velikosti geometry
+screen_width = ws.winfo_screenwidth()
+screen_height = ws.winfo_screenheight()
+vyska=630
+sirka=400
+center_x = int(screen_width/2 - vyska/ 2)
+center_y = int(screen_height/2 - sirka / 2)
+
+
+ws.geometry(f'{vyska}x{sirka}+{center_x}+{center_y}')
+ws.configure(cursor="top_left_corner",background="#262626")#"#004c7f")
+style = ttk.Style()
+style.theme_use("clam")
+ohraniceni = tk.Frame(ws, bg="white",height=100, width=100)
+ohraniceni.place(anchor=tk.W)
+
+bold = ("helvetica", 12, "bold")
+ws.option_add("*TCombobox*Listbox*Background", "white")
+ws.option_add("*TCombobox*Listbox*Foreground", "blue")
+ws.option_add("*TCombobox*Listbox*Font",bold)
+ws.option_add("*TCombobox*Listbox.selectBackground", "blue")
+
+ws.option_add("*TCombobox*Listbox.selectForeground", "white")
+
+style.map('TCombobox', fieldbackground=[('readonly', "blue")])
+style.map('TCombobox', selectbackground=[('readonly', "white")])
+style.map('TCombobox', selectforeground=[('readonly', "blue")])
+style.map('TCombobox', background=[('readonly', "white")])
+style.map('TCombobox', foreground=[('readonly', "white")])
+
+style = ttk.Style()
+style.theme_use("clam")
+style.map("Vertical.TScrollbar",
+          foreground=[("!active", "white"),("active", "blue")],
+          background=[("!active","white"),("active","white")])
+style.configure("Vertical.TScrollbar", troughcolor="blue",
+                lightcolor="blue")
+# style.configure('TCombobox',
+#                 background="white",
+#                 foreground="blue",
+#                 fieldbackground="white",
+#                 darkcolor="blue",
+#                 lightcolor="blue",
+#                 selectbackgroun="blue",
+#                 bordercolor="blue",
+#                 insertcolor="blue",
+#                 arrowcolor="blue")
+
 v1=tk.StringVar() # promena ze vstupu do druheho vstupniho pole
 v2=tk.StringVar()
 castkaA = tk.StringVar()
 kurzA = tk.StringVar()
 kurzB = tk.StringVar()
 kurzb = tk.StringVar()
-# popis pole
-l1 = tk.Label(ws, text="meny", font=("Arial", 25))
 
+seznam_fiat = ['AUD','GBP','CNY','DKK','EUR','HRK','JPY','CAD','HUF','NOK','PLN','RON','SEK','CHF','TRY','USD']
+#frame = tk.Frame(ws,relief="sunken", bg="#325bff")
+#nme = tk.Label(ws, text="Měny")
+fa_r = tk.LabelFrame(ws,text="Měny",
+                    fg="white",
+                    borderwidth=6,
+                    relief="sunken",
+                    #labelwidget=nme,
+                    bg="#515151",#"#325bff",
+                    font=("helvetica",15,"bold"))
+
+# label info autor a popis programu
+autor_info= tk.Label(fa_r, text="Autor info",cursor= "hand2",relief="sunken",bd=2,width=10,background="white",foreground= "red", font= ('Aerial',8,"bold","underline"))#515151
+autor_info.bind("<Button-1>", lambda e:open_url(e))
+#autor_info.bind("<Enter>", lambda e:open_url(e))
+autor_info.place(x=15,y=350)
+
+# seznam fiat men 
+listbox = ttk.Combobox(fa_r,state = "readonly",width=8,cursor= "hand2", style="my.TCombobox",values = seznam_fiat,font=("Arial", 15))
+listbox.set("Fiat")
+listbox.pack()
+
+
+
+# style.configure("my.TCombobox",backgroudn="black",
+#                 #fieldbackground="#1d2128",
+#                 insertwidth="red",
+#                 foreground="green",
+#                 darkcolor="lime",
+#                 selectbackground="green",
+#                 lightcolor="green")
+
+# seznam krypto men a paru 
+krypto_list = ttk.Combobox(fa_r,state = "readonly",width=8,style="my.TCombobox", values = krypto_seznam,font=("Arial", 15))
+#style.configure("K.TCombobox", selectbackground="blue")
+
+krypto_list.set("Krypto")
+krypto_list.pack(pady=40)
+fa_r.place(bordermode=tk.OUTSIDE,x=508,height=398)
+
+
+# nadpis pro labelframe
+pole1 = tk.Label(ws, text="Fiat")
+ram_fiat = tk.LabelFrame(ws,
+                   borderwidth=6,
+                   relief="sunken",
+                   labelwidget=pole1,
+                   bg="#515151",
+                   fg="white",
+                   font=("helvetica",15))
+
+ram_fiat.place(x=150,y=20)#bordermode=tk.OUTSIDE)
 # pole pro vstup od uzivatele
-entry1 =  tk.Entry(ws,font=("Arial",15),textvariable=v1)
+entry1 =  tk.Entry(ram_fiat,font=("Arial",15,"bold"),
+                   borderwidth=10,width=29,
+                   textvariable=v1,
+                   bg="blue",
+                   fg="white")
 #c,k,d,p,czk
-l2 = tk.Label(ws, text= "BTC", font=("Arial", 25))
-entry2 = tk.Entry(ws,font=("Arial",15),textvariable=v2)
+pole2 = tk.Label(ws, text="Krypto")
+ram_krypto = tk.LabelFrame(ws,
+                   borderwidth=6,
+                   relief="sunken",
+                   labelwidget=pole2,
+                   bg="#515151",
+                   fg="white",
+                   font=("helvetica",15))
 
-castka_a = tk.Label(ws, text="castka")
-castka_a.place(x=530, y=100)
-L3 = tk.Label(ws, underline=4, wraplength=70,textvariable=castkaA, relief="sunken")
-L3.place(x=600, y= 100)
+ 
+entry2 = tk.Entry(ram_krypto,font=("Arial",15,"bold"),cursor= "top_left_corner",
+                   borderwidth=10,width=29,
+                   textvariable=v2,
+                   bg="blue",
+                   fg="white")
+ram_krypto.place(x=150,y=120)
+entry1.grid(column=0, row=0)
+# krypto a pole vstupu
 
-kurz_1 = tk.Label(ws, text="kurz USD")
-kurz_1.place(x=530, y = 125)
-kurz = tk.Label(ws, textvariable=kurzA, relief="sunken")
-kurz.place(x=600, y=125)
+entry2.grid(column=1, row=1)
 
-kurz_b = tk.Label(ws, text="kurz")
-kurz_b.place(x=530, y=145)
-kurz_B=tk.Label(ws, textvariable=kurzB) # uzivatel vybral menu 
-kurz_B.place(x=560, y =145)
-da_mena= tk.Label(ws, textvariable=kurzb, relief="sunken")
-da_mena.place(x=600,y=145)
 
-l3 = tk.Label(ws, text="/", font=("Arial", 25))
+#nadpis_1 = tk.Label(ws, text="Podrobnosti",bg="green", width=30)
+nadpis = tk.Label(ws, text = "Podrobnosit",fg="white",
+                  width=20,
+                  bg="blue",
+                  borderwidth=6,
+                  relief="sunken",
+                  font=("helvetica",10,"bold"))
+l_f = tk.LabelFrame(ws,
+                   borderwidth=6,
+                   relief="sunken",
+                   #text="Podrobnosti",
+                   #relief="sunken",
+                   labelwidget=nadpis,
+                   bg="#515151",
+#                  fg="white",
+                   font=("helvetica",15,"bold"))
+
+
+l_f.place(bordermode=tk.OUTSIDE,x=150,y=267,width=355,height=130)
+castka_a = tk.Label(l_f, text="castka",bg="white",bd=5,
+                    relief="sunken",
+                    fg = "blue",
+                    font=("helvetica",10,"bold"))
+
+castka_a.grid(column=0, row=1)
+
+L3 = tk.Label(l_f, underline=4,
+              wraplength=70,
+              textvariable=castkaA,
+              bd=5,
+              bg="blue",
+              fg="white",
+              font=("helvetica", 10,"bold"),
+              relief="sunken")
+
+L3.grid(column=1, row=1,ipadx=5)
+
+kurz_1 = tk.Label(l_f, text="kurz",
+                  bg="blue", bd=5,
+                  relief="sunken",
+                  fg="white",
+                  font=("helvetica",10,"bold"))
+
+kurz_1.grid(column=0, row=2)
+
+kurz = tk.Label(l_f, textvariable=kurzA, bg="blue",
+                borderwidth=6,relief="sunken",
+                fg="white",
+                font=("helvetica",10,"bold"))
+
+kurz.grid(column=1, row=2,ipadx=5)
+
+kurz_b = tk.Label(l_f, text="Aktualni cena",bg="blue",
+                bd=5,relief="sunken",
+                fg="white",
+                font=("helvetica",10,"bold"))
+
+
+kurz_b.grid(column=0, row=3)
+
+kurz_B=tk.Label(l_f, textvariable=kurzB,bg="blue",
+                bd=5,relief="sunken",
+                fg="white",
+                font=("helvetica",10,"bold")) # uzivatel vybral menu 
+
+kurz_B.grid(column=2, row=2,ipadx=5)
+
+da_mena= tk.Label(l_f, textvariable=kurzb, bg="blue",
+                bd=5,relief="sunken",
+                fg="white",
+                font=("helvetica",10,"bold"))
+
+
+da_mena.grid(column=2, row=3,ipadx=5)
+
+# ws.state= ttk.Style()
+# style.configure("TButton", background="black")
+
+#ll = tk.Label(ws,text="Ovládání",fg="white",width=32, bg="red")
+
+fr = tk.LabelFrame(ws,
+                   borderwidth=6,
+                   text="Ovladani",
+                   relief="sunken",
+                   #labelwidget=ll,
+                   bg="#515151",#"#325bff",
+                   fg="white",
+                   font=("helvetica",15))
+
+fr.place(bordermode=tk.OUTSIDE, height=398)#grid(column=0,row=0,pady=10,padx=30)
+
+style.configure("C.TButton",borderwidth=2 ,font=("calibri", 10, "bold","underline"))
+style.map("C.TButton",
+          
+          foreground=[("!active", "blue"),("active", "white")],
+          background=[("!active","white"),("active","blue")])
+style.configure("B.TButton", font=("calibri", 10,"bold","underline"))#borderwidth=2
+style.map("B.TButton",
+            foreground=[("!active", "white"),("active", "blue")],
+            background=[("!active","blue"),("active","white")])
 
 # tlacitka a volani na funkce 
-f_b = tk.Button(ws,text="Fiat/BTC", command=fiat_krypto) # nedavat () jinak se funkce zavola automaticky
-b_f = tk.Button(ws, text="BTC/Fiat" ,command=krypto_fiat)
-b_s = tk.Button(ws, text="Smazat", command=smaz)
-b_e = ttk.Button(ws, text="Exit", command=exit)
+f_b = ttk.Button(fr,text="Fiat/BTC",style="C.TButton",command=fiat_krypto) # nedavat () jinak se funkce zavola automaticky
+b_f = ttk.Button(fr, text="BTC/Fiat",style="B.TButton" ,command=krypto_fiat)
+b_s = ttk.Button(fr, text="Smazat",style="C.TButton", command=smaz)
+b_e = ttk.Button(fr, text="Exit",style="B.TButton", command=exit)
+b_r = ttk.Button(fr, text="Refresh", style="C.TButton", command=refresh)
 # fiat popis a pole vstupu
-#l1.place(x=15,y=20)
-entry1.place(x=130, y=26)
-
+entry1.grid(column=1, row=1)
 # krypto a pole vstupu
-#l2.place(x=730, y=20)
-entry2.place(x=380, y=26)
+entry2.grid(column=2, row=1)
 
-l3.place(x=360,  y=20)
-# tlacitka
-f_b.place(x=50, y=100)
-b_f.place(x=140, y = 100)
-b_s.place(x=230, y=100)
-b_e.place(x=320, y = 100)
+
+f_b.grid(column=1, row=1 ,pady=10,ipadx=10, ipady=10)# fiat/btc
+b_f.grid(column=1, row=2,pady=10,ipadx=10, ipady=10)# btc/fiat
+b_s.grid(column=1, row=3,pady=10,ipadx=10, ipady=10)# smaz
+b_e.grid(column=1, row=4,pady=10,ipadx=10, ipady=10)# exit
+b_r.grid(column=1, row=5,pady=10,ipadx=10, ipady=10)#refresh
+
+
+#
 #listbox.place(x=15,y=22)
 ws.mainloop()
